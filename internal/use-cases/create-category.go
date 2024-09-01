743e309f1e29d8f853a8e88ad5e7eaecc9526051
ghenoo <lucasgheno600@gmail.com>
2024-09-01 15:27:22 -0300

    chore: add category

--- internal/use-cases/create-category.go
+++ internal/use-cases/create-category.go
@@ -1,4 +1,25 @@
 package use_cases
 
-type CreateCategoryUseCase struct {
+import (
+	"github.com/ghenoo/microservices-go/internal/repositories"
+
+	"github.com/ghenoo/microservices-go/internal/entities"
+)
+
+type listCategoriesUseCase struct {
+	repository repositories.ICategoryRepository
+}
+
+func NewListCategoriesUseCase(repository repositories.ICategoryRepository) *listCategoriesUseCase {
+	return &listCategoriesUseCase{repository}
+}
+
+func (u *listCategoriesUseCase) Execute() ([]*entities.Category, error) {
+	categories, err := u.repository.List()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return categories, nil
 }
