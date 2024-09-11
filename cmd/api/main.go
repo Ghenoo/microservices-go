25308c6c1184c2d1bd3fbfb721d4982dfe2f9862
Ghenoo <lucasgheno600@gmail.com>
2024-09-11 14:39:53 -0300

    fix: update main for connect if kafka

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,16 +6,23 @@ import (
 
 	"github.com/ghenoo/microservices-go/cmd/db"
 	"github.com/ghenoo/microservices-go/cmd/handlers"
+	"github.com/ghenoo/microservices-go/cmd/producer"
 )
 
-
 func main() {
-	db.InitDB()
-    handlers.SetupRoutes()
 
-    log.Println("Server is starting on port 8080...")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatalf("Failed to start the server: %v", err)
-    }
+	db.InitDB()
+
+	writer := producer.NewKafkaWriter("localhost:9092", "meu-topico")
+	defer writer.Close()
+
+	handlers.KafkaWriter = writer
+
+	handlers.SetupRoutes()
+
+	log.Println("Server is starting on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
