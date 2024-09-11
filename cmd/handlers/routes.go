1b591f81142da86a7d53fe4b35ee4349058c9773
Ghenoo <lucasgheno600@gmail.com>
2024-09-11 14:41:21 -0300

    fix: update the routes for connection if kafka

--- cmd/handlers/routes.go
+++ cmd/handlers/routes.go
@@ -1,8 +1,35 @@
 package handlers
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/ghenoo/microservices-go/cmd/producer"
+)
+
+var KafkaWriter *producer.KafkaWriter
 
 func SetupRoutes() {
-    http.HandleFunc("/users", GetUsersHandler)
-    http.HandleFunc("/users/add", PostUserHandler)
+	http.HandleFunc("/users", GetUsersHandler)
+	http.HandleFunc("/users/add", PostUserHandler)
+
+	// Novas rotas
+	http.HandleFunc("/send-message", SendMessageHandler)
+	http.HandleFunc("/status", StatusHandler)
+}
+
+func SendMessageHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Mensagem enviada para o Kafka!"
+	err := KafkaWriter.WriteMessages(context.Background(),
+		producer.NewMessage([]byte("Key"), []byte(message)),
+	)
+	if err != nil {
+		http.Error(w, "Failed to send message to Kafka", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, message)
+}
+func StatusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Servidor está funcionando!")
 }
